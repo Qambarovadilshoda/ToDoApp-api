f4aee146e6fa126408b5a429cdae99a6fcbafd3a
Dilshoda <qambarovadilshoda867@gmail.com>
2024-11-19 15:07:31 +0500

    I added resource route for tasks to api routes

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,16 @@
 <?php
 
-use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TaskController;
 
 Route::middleware('auth:sanctum')->group(function(){
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
     Route::get('/logout', [AuthController::class, 'logout']);
+    Route::apiResource('/tasks', TaskController::class);
 });
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
