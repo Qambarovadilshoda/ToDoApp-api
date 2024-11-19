8a426e53229cd60fa189de0f7d007099d994c927
Dilshoda <qambarovadilshoda867@gmail.com>
2024-11-19 17:23:10 +0500

    I added route search and mark-done

--- routes/api.php
+++ routes/api.php
@@ -11,7 +11,9 @@ Route::middleware('auth:sanctum')->group(function(){
     });
     Route::get('/logout', [AuthController::class, 'logout']);
     Route::apiResource('/tasks', TaskController::class);
+    Route::get('/mark-done/{id}', [TaskController::class, 'markDone']);
 });
+Route::get('/search', [TaskController::class, 'search']);
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/verify-email', [AuthController::class, 'verifyEmail'])->name('email.verify');
